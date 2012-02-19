34bc64b1d6fc340fdacacda4eedb817cc5e0083e
Michael S. Liebman <msl521@users.sourceforge.net>
2012-02-19 14:32:11 -0500

    Provider
    [in src/main/java/com/github/msl521/met/domain/]
    * Provider.java,
      Provider_Roo_Configurable.aj,
      Provider_Roo_Entity.aj,
      Provider_Roo_ToString.aj:
      Initial add.
    
    [in src/test/java/com/github/msl521/met/domain/]
    * ProviderDataOnDemand.java,
      ProviderDataOnDemand_Roo_Configurable.aj,
      ProviderDataOnDemand_Roo_DataOnDemand.aj,
      ProviderIntegrationTest.java,
      ProviderIntegrationTest_Roo_Configurable.aj,
      ProviderIntegrationTest_Roo_IntegrationTest.aj:
      Initial add.

---
+++ src/main/java/com/github/msl521/met/domain/Provider.java
@@ -0,0 +1,11 @@
+package com.github.msl521.met.domain;
+
+import org.springframework.roo.addon.entity.RooEntity;
+import org.springframework.roo.addon.javabean.RooJavaBean;
+import org.springframework.roo.addon.tostring.RooToString;
+
+@RooJavaBean
+@RooToString
+@RooEntity
+public class Provider extends Organization {
+}
---
+++ src/test/java/com/github/msl521/met/domain/ProviderDataOnDemand.java
@@ -0,0 +1,7 @@
+package com.github.msl521.met.domain;
+
+import org.springframework.roo.addon.dod.RooDataOnDemand;
+
+@RooDataOnDemand(entity = Provider.class)
+public class ProviderDataOnDemand {
+}
