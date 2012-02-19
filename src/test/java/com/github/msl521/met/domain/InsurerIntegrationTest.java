f2978adfc8071125ea236d6fe9f57705fb43d70b
Michael S. Liebman <msl521@users.sourceforge.net>
2012-02-19 14:34:51 -0500

    Insurer
    [in src/main/java/com/github/msl521/met/domain/]
    * Insurer.java,
      Insurer_Roo_Configurable.aj,
      Insurer_Roo_Entity.aj,
      Insurer_Roo_ToString.aj:
      Initial add.
      
    [in src/test/java/com/github/msl521/met/domain/]
    * InsurerDataOnDemand.java,
      InsurerDataOnDemand_Roo_Configurable.aj,
      InsurerDataOnDemand_Roo_DataOnDemand.aj,
      InsurerIntegrationTest.java,
      InsurerIntegrationTest_Roo_Configurable.aj,
      InsurerIntegrationTest_Roo_IntegrationTest.aj:
      Initial add.

---
+++ src/test/java/com/github/msl521/met/domain/InsurerIntegrationTest.java
@@ -0,0 +1,12 @@
+package com.github.msl521.met.domain;
+
+import org.junit.Test;
+import org.springframework.roo.addon.test.RooIntegrationTest;
+
+@RooIntegrationTest(entity = Insurer.class)
+public class InsurerIntegrationTest {
+
+    @Test
+    public void testMarkerMethod() {
+    }
+}
