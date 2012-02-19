344a8b02244c86baa74634ce1706d4019cb474ee
Michael S. Liebman <msl521@users.sourceforge.net>
2012-02-19 15:13:33 -0500

    Provider Type
    * src/main/java/com/github/msl521/met/domain/Provider.java:
      Make provider type required.

--- src/main/java/com/github/msl521/met/domain/Provider.java
+++ src/main/java/com/github/msl521/met/domain/Provider.java
@@ -1,16 +1,20 @@
 package com.github.msl521.met.domain;
 
 import org.springframework.roo.addon.entity.RooEntity;
+
 import org.springframework.roo.addon.javabean.RooJavaBean;
 import org.springframework.roo.addon.tostring.RooToString;
 import com.github.msl521.met.domain.ProviderType;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
 
 @RooJavaBean
 @RooToString
 @RooEntity
 public class Provider extends Organization {
 
+	@NotNull
     @ManyToOne
     private ProviderType providerType;
 }
